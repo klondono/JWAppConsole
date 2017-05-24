3325fc1c228e83f978f4da6402230640f95b4fc3
Kerwin Londono <klondono@Kerwins-MacBook-Pro.local>
2017-05-24 19:24:29 -0400

    Request token send request with token

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
 namespace JWAppConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+        private static async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            //The last step is now to call the API.
+            //To send the access token to the API you typically use the HTTP Authorization header. 
+            //This is done using the SetBearerToken extension method
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
